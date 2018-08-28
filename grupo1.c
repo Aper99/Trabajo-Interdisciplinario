c37b73d96925cc0ed309cd5db2b58b584c2ed081
Aper99 <alexpenciso@gmail.com>
2018-08-27 23:56:55 -0400

    Version 1
    Agregado conversion de decimal a binario
    Falta octal.

--- grupo1.c
+++ grupo1.c
@@ -3,8 +3,33 @@
 
 int main(){
 	
+	int n, x,c=1,aux;
 	
+	printf("Ingrese un numero: \n");
+	scanf("%i",&n);
+	aux = n;
+	while ( (n / 2) > 0)
+	{
+		c++;
+		n = n / 2;
+	}
+	printf("%i\n",c);
 	
+	n = aux;
+	int aux2= c;
 	
+	int b[c];
+	x = 0;
+	while(n>0)
+	{
+		b[x] = n % 2;
+		n = n / 2;
+		x++;
+	}
+
+	for (x=aux2-1;x>=0;x--)
+	{
+		printf("%i",b[x]);
+	}
 	return 0;
 }
