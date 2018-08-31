562c1e0301c14647906dfd15b7a0d62217f83a26
SaidaPereira <42759887+SaidaPereira@users.noreply.github.com>
2018-08-31 14:18:44 -0400

    Update grupo2.c
    Se ingresa un valor en decimal y muestra el resultado en Binario.
    Se ingresa otro valor en decimal y muestra el resultado en Hexadecimal

--- grupo2.c
+++ grupo2.c
@@ -1,20 +1,62 @@
-//Grupo 2 : Conversor/Ingresa Decimal : Salida Binario - Hexadecimal 
-#include <stdio.h>
-void binario (int N);
+//Grupo 2 : Conversor/Ingresa Decimal : Salida Binario - Hexadecimal
+#include<stdio.h>
 int main(){
- int N;
- int aux;
-    aux=N;
-    do{
-   	printf("Digite un numero : ");
-   	scanf("%d",&N);
-	}while (N<0);
-	
-	binario(N);
-	return (0);	
-}  
-   void binario(int N){
-   	if (N>1) binario(N/2);
-   	printf("%d",N%2);
-   }  
-//
+	int numero;
+    int hexadecimales(int numero);
+    char hexadecimal[16]={'1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+    int i=0; 
+    int binarios(int numero);
+    char binario[2]={'0','1'};
+    int c=0;
+  
+    printf("Ingrese un valor ");
+	scanf("%d",&numero);
+    while (numero!=0){
+         binario[c]=numero % 2;
+          numero=numero / 2;
+          c++;
+    }
+    c--;
+    printf("Binario: ");   
+    while (c>=0)
+    {
+          printf("%u",binario[c]);
+          c--;
+      }
+
+    
+	printf("Ingrese un valor ");
+	scanf("%d",&numero);
+    
+	while (numero!=0){
+           hexadecimal[i]=numero%16;
+           numero=numero/16;
+           i++;}
+           
+     printf("\nHexadecimal: ");
+     i--;
+     
+     while (i>=0){
+     switch(hexadecimal[i]){
+     case 10:
+     printf("A");break;
+     case 11:
+     printf("B");break;
+     case 12:
+     printf("C");break;
+     case 13:
+     printf("D");break;
+     case 14:
+     printf("E");break;
+     case 15:
+     printf("F");break;
+     default:
+     printf("%d",hexadecimal[i]);break;
+    } 
+    i--;
+    
+   }
+    	
+   getch();
+   return(0);
+}
