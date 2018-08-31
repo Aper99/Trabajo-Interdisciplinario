1cfcbe6504574ad34e34ac1e1b7bee88f495c6d8
SaidaPereira <42759887+SaidaPereira@users.noreply.github.com>
2018-08-31 10:33:28 -0400

    Update grupo2.c

--- grupo2.c
+++ grupo2.c
@@ -1,10 +1,19 @@
 //Grupo 2 : Conversor/Ingresa Decimal : Salida Binario - Hexadecimal 
 #include <stdio.h>
-
+void binario (int N);
 int main(){
+ int N;
+ int aux;
+    aux=N;
+    do{
+   	printf("Digite un numero : ");
+   	scanf("%d",&N);
+	}while (N<0);
 	
-	
-	
-	
-	return 0;
-}
+	binario(N);
+	return (0);	
+}  
+   void binario(int N){
+   	if (N>1) binario(N/2);
+   	printf("%d",N%2);
+   }  
