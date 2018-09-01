206af0a52fbade237a663022b4af4d5806b616c0
pablodenis7 <42759958+pablodenis7@users.noreply.github.com>
2018-09-01 11:26:28 -0400

    Update grupo2.c

--- grupo2.c
+++ grupo2.c
@@ -1,10 +1,87 @@
 //Grupo 2 : Conversor/Ingresa Decimal : Salida Binario - Hexadecimal 
-#include <stdio.h>
 
-int main(){
-	
-	
-	
-	
-	return 0;
+#include <stdio.h>
+#include <conio.h>
+
+short i;
+unsigned short binario[7];
+unsigned short hexadecimal[10];
+
+int main()
+{
+   
+    int numero=0;
+    
+    int binarios(int); 
+    int hexadecimales(int);
+   
+   
+    printf("Ingrese valor a convertir: ");
+    scanf("%u",&numero);
+    
+    
+    binarios(numero);
+    hexadecimales(numero);
+    getch();
+    return 0;
+}
+
+int binarios(int numero)
+{
+    i=0; 
+    
+    while (numero!=0)
+    {
+          binario[i]=numero % 2;
+          numero=numero / 2;
+          i++;
+    }
+    i--;
+    printf("Binario: ");   
+    
+    while (i>=0)
+    {
+          printf("%u",binario[i]);
+          i--;
+    }
+}
+ int hexadecimales(int numero)
+ {
+     i=0;
+     while (numero!=0)
+     {
+           hexadecimal[i]=numero%16;
+           numero=numero/16;
+           i++;
+     }
+     printf("\nHexadecimal: ");
+     i--;
+     while (i>=0)
+     {
+           switch(hexadecimal[i])
+           {
+                             case 10:
+                                  printf("A");
+                             break;
+                             case 11:
+                                  printf("B");
+                             break;
+                             case 12:
+                                  printf("C");
+                             break;
+                             case 13:
+                                  printf("D");
+                             break;
+                             case 14:
+                                  printf("E");
+                             break;
+                             case 15:
+                                  printf("F");
+                             break;
+                             default:
+                                   printf("%d",hexadecimal[i]);
+                             break;
+           } 
+           i--;
+     }
 }
