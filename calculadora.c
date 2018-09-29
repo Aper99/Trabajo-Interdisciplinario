a495146923232da1e0d56a5852b1808245f0d32a
Aper99 <alexpenciso@gmail.com>
2018-09-29 16:17:07 -0400

    ver. 2.1
    Agregado entrada octal y hexadecimal

--- calculadora.c
+++ calculadora.c
@@ -8,6 +8,8 @@ void binario();
 void hex();
 void octal();
 int binarioA10();
+int OctalA10();
+int HexA10();
 
 //Funcion Principal
 int main(){
@@ -19,14 +21,16 @@ int main(){
 		switch(opcion){
 			case 1 : num = leernumero(); binario(num); octal(num); hex(num); break;//Entrada decimal
 			case 2 : x = binarioA10(); octal(x); hex(x); break;//Entrada Binario
-			case 3 : goto salir;//Finaliza directamente el programa
-			default : printf("\nOpcion no valida");
+			case 3 : x = OctalA10(); binario(x); hex(x); break;
+			case 4 : x = HexA10(); binario(x); octal(x); break;
+			case 5 : return 0;//Finaliza directamente el programa
+			default : printf("\n\n\tOpcion no valida");
 			
 		}
 		printf("\n\n\n\n\t %cDesea convertir otro numero?  \n\n\t 1- Si 2- No \n\n\t Opcion: ",168);
 		scanf("%d",&j);
 	}
-	salir:
+	
 	return 0;
 }
 
@@ -43,7 +47,9 @@ int menu(){//Opciones del Menu
 	printf("\n\n\t Seleccione la base ");
 	printf("\n\n\t 1.Entrada Decimal ");
 	printf("\n\n\t 2.Entrada Binario");
-	printf("\n\n\t 3.Salir ");
+	printf("\n\n\t 3.Entrada Octal");
+	printf("\n\n\t 4.Entrada Hexadecimal");
+	printf("\n\n\t 5.Salir ");
 	printf("\n\n\t Opcion: ");
 	scanf("%d",&x);
 	return x;
@@ -65,53 +71,57 @@ void binario(int numero){// Funcion Conversor
     }
 }
 void octal(int x){// Funcion Conversor
-	int c=1, i;
-	int b = 0;
-	int aux = x;
-	while (x>=8)
-	{
-		c++;
-		x /= 8;
+	if (x != -1) {
+		int c=1, i;
+		int b = 0;
+		int aux = x;
+		while (x>=8)
+		{
+			c++;
+			x /= 8;
+		}
+		for (i=0;i<c;i++)
+		{
+			b += (aux % 8)*pow(10,i);
+			aux /= 8;
+		}
+		printf("\n\n\t Octal: %d",b);
 	}
-	for (i=0;i<c;i++)
-	{
-		b += (aux % 8)*pow(10,i);
-		aux /= 8;
-	}
-	printf("\n\n\t Octal: %d",b);
 }
 void hex(int numero){// Funcion Conversor
-	int c=0, i =0;
-	char hexadecimal[16]={'1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-
-	while (numero!=0){
-           hexadecimal[i]=numero%16;
-           numero=numero/16;
-           i++;
+	if (numero != -1){
+		int c=0, i =0;
+		char hexadecimal[16]={'1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+	
+		while (numero!=0){
+	           hexadecimal[i]=numero%16;
+	           numero=numero/16;
+	           i++;
+		}
+	           
+	     printf("\n\n\t Hexadecimal: ");
+	     i--;
+	     
+	     while (i>=0){
+	     switch(hexadecimal[i]){
+	     case 10:
+	     printf("A");break;
+	     case 11:
+	     printf("B");break;
+	     case 12:
+	     printf("C");break;
+	     case 13:
+	     printf("D");break;
+	     case 14:
+	     printf("E");break;
+	     case 15:
+	     printf("F");break;
+	     default:
+	     printf("%d",hexadecimal[i]);break;
+	    } 
+	    i--;
+	    }
 	}
-           
-     printf("\n\n\t Hexadecimal: ");
-     i--;
-     
-     while (i>=0){
-     switch(hexadecimal[i]){
-     case 10:
-     printf("A");break;
-     case 11:
-     printf("B");break;
-     case 12:
-     printf("C");break;
-     case 13:
-     printf("D");break;
-     case 14:
-     printf("E");break;
-     case 15:
-     printf("F");break;
-     default:
-     printf("%d",hexadecimal[i]);break;
-    } 
-    i--;
-    }
 }
 
 int binarioA10(){// Funcion Conversor
@@ -139,6 +149,7 @@ int binarioA10(){// Funcion Conversor
 	if (y == -1)
 	{
 		printf("\n\n\t El numero no es binario ");
+		return -1;
 	}
 	else
 	{
@@ -146,5 +157,76 @@ int binarioA10(){// Funcion Conversor
 		return y;	
 	}
 	
+}
+
+int OctalA10(){
+	char oct[100];
+	int i=0, x, num = 0;
+	printf("\n\n\t Ingrese un numero Octal ");
+	fflush(stdin);
+	gets(oct);
+	while (oct[i] != '\0')
+	{
+		switch (oct[i])
+		{
+			case '0': x = 0; break;
+			case '1': x = 1; break;
+			case '2': x = 2; break;
+			case '3': x = 3; break;
+			case '4': x = 4; break;
+			case '5': x = 5; break;
+			case '6': x = 6; break;
+			case '7': x = 7; break;
+			default : printf("\n\n\t No es un numero Octal"); return -1;
+		}
+		num = x + (num * 8);
+		i++;
+	}
+	
+
+	printf("\n\n\t Decimal: %d",num);
+	return num;	
+}
+
+int HexA10(){
+	char hex[100];
+	int i=0, x, num = 0;
+	printf("\n\n\t Ingrese un numero Hexadecimal ");
+	fflush(stdin);
+	gets(hex);
+	
+	while (hex[i] != '\0')
+	{
+		switch (hex[i])
+		{
+			case '0': x = 0; break;
+			case '1': x = 1; break;
+			case '2': x = 2; break;
+			case '3': x = 3; break;
+			case '4': x = 4; break;
+			case '5': x = 5; break;
+			case '6': x = 6; break;
+			case '7': x = 7; break;
+			case '8': x = 8; break;
+			case '9': x = 9; break;
+			case 'A':
+			case 'a': x = 10; break;
+			case 'B':
+			case 'b': x = 11; break;
+			case 'C':
+			case 'c': x = 12; break;
+			case 'D':
+			case 'd': x = 13; break;
+			case 'E':
+			case 'e': x = 14; break;
+			case 'F':
+			case 'f': x = 15; break;
+			default : printf("\nNo es un numero Hexadecimal"); return -1;
+		}
+		num = x + (num * 16);
+		i++;
+	}
+	printf("\n\n\t Decimal: %d",num);
+	return num;	
 }
 
